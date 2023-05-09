bc031ebe5d68cd7084e5c91a168da8857236ef00
Dias <l1verpoolg24@gmail.com>
2023-05-09 10:56:26 +0600

    completed styles for about, career and footer sections

--- src/index.js
+++ src/index.js
@@ -3,6 +3,36 @@ import "./index.html";
 import "swiper/scss";
 import "swiper/scss/pagination";
 import "./index.scss";
+import { slidersInit } from "./modules/sliders";
+
+slidersInit(".about__slider", {
+  pagination: {
+    el: ".about__slider-pagination",
+  },
+});
+
+slidersInit(".career__slider", {
+  pagination: {
+    el: ".career__slider-pagination",
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: "auto",
+      spaceBetween: 20,
+      pagination: false,
+    },
+    1024: {
+      slidesPerView: "auto",
+      spaceBetween: 26,
+      pagination: false,
+    },
+    1240: {
+      slidesPerView: "auto",
+      spaceBetween: 30,
+      pagination: false,
+    },
+  },
+});
 
 const videoBG = document.querySelector(".video-bg");
 
---
+++ src/modules/sliders.js
@@ -0,0 +1,22 @@
+import { Swiper, Pagination, Autoplay } from "swiper";
+
+const params = {
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+  slidesPerView: 1,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  modules: [Autoplay, Pagination],
+};
+
+export const slidersInit = (selectorSlider, newParams) => {
+  new Swiper(selectorSlider, {
+    ...params,
+    ...newParams,
+  });
+};
